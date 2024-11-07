0d9d2315ace7ebd26d6b7710e9340361be1337d0
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-07 06:45:06 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//*****************************************************************************
+//** 2275. Largest Combination With Bitwise AND Greater Than Zero   leetcode **
+//*****************************************************************************
+
+
+int largestCombination(int* candidates, int candidatesSize) {
+    const int BITS = 24;
+    
+    int maxCount = 0;
+
+    for (int i = 0; i < BITS; i++) {
+        int bitCount = 0; 
+
+        for (int j = 0; j < candidatesSize; j++) {
+            if ((candidates[j] >> i) & 1) {
+                bitCount++;
+            }
+        }
+        
+        if (bitCount > maxCount) {
+            maxCount = bitCount;
+        }
+    }
+    
+    return maxCount;
+}
